cb8fc3a2c0dcef70894675d29e363ae82a480120
Christopher Lawless <lawless85@gmail.com>
2016-05-28 13:36:03 +0100

    contining setup

--- app/src/main/java/lalwess/fluidsolver/PostProcessHandler.java
+++ app/src/main/java/lalwess/fluidsolver/PostProcessHandler.java
@@ -13,26 +13,27 @@ import com.threed.jpct.*;
 public class PostProcessHandler {
 
     public NPOTTexture processingTexture;
-   // public NPOTTexture mainTexture;
+
+    public NPOTTexture velocity;
+    public NPOTTexture density;
+    public NPOTTexture pressure;
+    public NPOTTexture diffusion;
+    public NPOTTexture divergence;
+    public NPOTTexture vorticity;
 
 
     World world;
 
     World AddingWorld;
     Camera cama = null;
-
     World AdvectingWorld;
     Camera camb = null;
-
     World BoundaryWorld;
     Camera camc = null;
-
     World divergenceWorld;
     Camera camd = null;
-
     World gradientWorld;
     Camera came = null;
-
     World jacobiWorld;
     Camera camf = null;
 
@@ -78,6 +79,14 @@ public class PostProcessHandler {
     TextureManager tm = TextureManager.getInstance();
     TextureInfo screens_ti;
 
+    TextureInfo adding_ti;
+    TextureInfo advecting_ti;
+    TextureInfo boundary_ti;
+    TextureInfo divergence_ti;
+    TextureInfo gradient_ti;
+    TextureInfo jacobi_ti;
+
+
     GLSLShader gameboy_shader  = null;
 
 
@@ -85,98 +94,19 @@ public class PostProcessHandler {
 
 
 
-    public void setUpCameras()
-    {
 
-
-        AddingWorld = new World();
-        AddingWorld.getCamera();
-        cama.setPosition(-10, 0, 0);
-        cama.lookAt(new SimpleVector(0, 0, 0));
-
-
-        AdvectingWorld = new World();
-        AdvectingWorld.getCamera();
-        camb.setPosition(-10, 0, 0);
-        camb.lookAt(new SimpleVector(0, 0, 0));
-
-
-        BoundaryWorld = new World();
-        BoundaryWorld.getCamera();
-        camc.setPosition(-10, 0, 0);
-        camc.lookAt(new SimpleVector(0, 0, 0));
-
-
-        divergenceWorld = new World();
-        divergenceWorld.getCamera();
-        camd.setPosition(-10, 0, 0);
-        camd.lookAt(new SimpleVector(0, 0, 0));
-
-        gradientWorld = new World();
-        gradientWorld.getCamera();
-        came.setPosition(-10, 0, 0);
-        came.lookAt(new SimpleVector(0, 0, 0));
-
-        postProcessWorld = new World();
-        postProcessWorld.getCamera();
-        camf.setPosition(-10, 0, 0);
-        camf.lookAt(new SimpleVector(0, 0, 0));
-
-    }
-
-    public void loadShaders(Resources res)
-    {
-
-        renderShader = new GLSLShader(Loader.loadTextFile(res.openRawResource(R.raw.mainvert)),
-        Loader.loadTextFile(res.openRawResource(R.raw.rendering_frag)));
-
-        loopingshader = new GLSLShader(Loader.loadTextFile(res.openRawResource(R.raw.mainvert)),
-                Loader.loadTextFile(res.openRawResource(R.raw.tapadd_frag)));
-
-
-
-
-         addingShader = new GLSLShader(Loader.loadTextFile(res.openRawResource(R.raw.mainvert)),
-                 Loader.loadTextFile(res.openRawResource(R.raw.tapadd_frag)));
-
-
-
-        advectingShader = new GLSLShader(Loader.loadTextFile(res.openRawResource(R.raw.mainvert)),
-                Loader.loadTextFile(res.openRawResource(R.raw.tapadd_frag)));
-
-
-
-        boundaryShader = new GLSLShader(Loader.loadTextFile(res.openRawResource(R.raw.mainvert)),
-                Loader.loadTextFile(res.openRawResource(R.raw.tapadd_frag)));
-
-
-
-        divergenceShader = new GLSLShader(Loader.loadTextFile(res.openRawResource(R.raw.mainvert)),
-                Loader.loadTextFile(res.openRawResource(R.raw.tapadd_frag)));
-
-
-
-        gradientShader = new GLSLShader(Loader.loadTextFile(res.openRawResource(R.raw.mainvert)),
-                Loader.loadTextFile(res.openRawResource(R.raw.tapadd_frag)));
-
-
-
-        jacobiShader = new GLSLShader(Loader.loadTextFile(res.openRawResource(R.raw.mainvert)),
-                Loader.loadTextFile(res.openRawResource(R.raw.tapadd_frag)));
-
-
-
-
-
-
-
-    }
 
 
 
     public PostProcessHandler(World world, Resources res, FrameBuffer fb) {
 
 
+
+
+
+      setUpCameras();
+      setupTextures(fb);
+    loadShaders(res);
 
 
         this.theRenderspot= Primitives.getPlane(4,10);
@@ -189,7 +119,7 @@ public class PostProcessHandler {
         cam.setPosition(-10, 0, 0);
         cam.lookAt(new SimpleVector(0, 0, 0));
 
-      loadShaders(res);
+
 
 
         processingTexture = new NPOTTexture(fb.getWidth() , fb.getHeight(), RGBColor.GREEN);
@@ -277,6 +207,142 @@ public class PostProcessHandler {
 
     }
 
+
+
+    public void setupTextures(FrameBuffer fb)
+    {
+
+        velocity = new NPOTTexture(fb.getWidth() , fb.getHeight(), RGBColor.GREEN);
+        velocity.setFiltering(true);
+        velocity.setMipmap(false);
+        velocity.setTextureCompression(true);//turning on texture compression eliminates the artifacts, no idea why lol
+        tm.addTexture("velocityTexture", velocity);
+
+        density = new NPOTTexture(fb.getWidth() , fb.getHeight(), RGBColor.GREEN);
+        density.setFiltering(true);
+        density.setMipmap(false);
+        density.setTextureCompression(true);//turning on texture compression eliminates the artifacts, no idea why lol
+        tm.addTexture("densityTexture", density);
+
+        pressure = new NPOTTexture(fb.getWidth() , fb.getHeight(), RGBColor.GREEN);
+        pressure.setFiltering(true);
+        pressure.setMipmap(false);
+        pressure.setTextureCompression(true);//turning on texture compression eliminates the artifacts, no idea why lol
+        tm.addTexture("pressureTexture", pressure);
+
+        diffusion = new NPOTTexture(fb.getWidth() , fb.getHeight(), RGBColor.GREEN);
+        diffusion.setFiltering(true);
+        diffusion.setMipmap(false);
+        diffusion.setTextureCompression(true);//turning on texture compression eliminates the artifacts, no idea why lol
+        tm.addTexture("diffusionTexture", diffusion);
+
+        divergence = new NPOTTexture(fb.getWidth() , fb.getHeight(), RGBColor.GREEN);
+        divergence.setFiltering(true);
+        divergence.setMipmap(false);
+        divergence.setTextureCompression(true);//turning on texture compression eliminates the artifacts, no idea why lol
+        tm.addTexture("divergenceTexture", divergence);
+
+        vorticity = new NPOTTexture(fb.getWidth() , fb.getHeight(), RGBColor.GREEN);
+        vorticity.setFiltering(true);
+        vorticity.setMipmap(false);
+        vorticity.setTextureCompression(true);//turning on texture compression eliminates the artifacts, no idea why lol
+        tm.addTexture("vorticityTexture", vorticity);
+    }
+
+    public void setUpCameras()
+    {
+
+
+        AddingWorld = new World();
+        cama=AddingWorld.getCamera();
+        cama.setPosition(-10, 0, 0);
+        cama.lookAt(new SimpleVector(0, 0, 0));
+
+
+        AdvectingWorld = new World();
+        camb=AdvectingWorld.getCamera();
+        camb.setPosition(-10, 0, 0);
+        camb.lookAt(new SimpleVector(0, 0, 0));
+
+
+        BoundaryWorld = new World();
+        camc=BoundaryWorld.getCamera();
+        camc.setPosition(-10, 0, 0);
+        camc.lookAt(new SimpleVector(0, 0, 0));
+
+
+        divergenceWorld = new World();
+        camd=divergenceWorld.getCamera();
+        camd.setPosition(-10, 0, 0);
+        camd.lookAt(new SimpleVector(0, 0, 0));
+
+        gradientWorld = new World();
+        came=gradientWorld.getCamera();
+        came.setPosition(-10, 0, 0);
+        came.lookAt(new SimpleVector(0, 0, 0));
+
+
+
+
+        jacobiWorld = new World();
+        camf=jacobiWorld.getCamera();
+        camf.setPosition(-10, 0, 0);
+        camf.lookAt(new SimpleVector(0, 0, 0));
+
+
+
+
+
+
+    }
+
+    public void loadShaders(Resources res)
+    {
+
+        renderShader = new GLSLShader(Loader.loadTextFile(res.openRawResource(R.raw.mainvert)),
+                Loader.loadTextFile(res.openRawResource(R.raw.rendering_frag)));
+
+        loopingshader = new GLSLShader(Loader.loadTextFile(res.openRawResource(R.raw.mainvert)),
+                Loader.loadTextFile(res.openRawResource(R.raw.tapadd_frag)));
+
+
+
+
+        addingShader = new GLSLShader(Loader.loadTextFile(res.openRawResource(R.raw.mainvert)),
+                Loader.loadTextFile(res.openRawResource(R.raw.tapadd_frag)));
+
+
+
+        advectingShader = new GLSLShader(Loader.loadTextFile(res.openRawResource(R.raw.mainvert)),
+                Loader.loadTextFile(res.openRawResource(R.raw.tapadd_frag)));
+
+
+
+        boundaryShader = new GLSLShader(Loader.loadTextFile(res.openRawResource(R.raw.mainvert)),
+                Loader.loadTextFile(res.openRawResource(R.raw.tapadd_frag)));
+
+
+
+        divergenceShader = new GLSLShader(Loader.loadTextFile(res.openRawResource(R.raw.mainvert)),
+                Loader.loadTextFile(res.openRawResource(R.raw.tapadd_frag)));
+
+
+
+        gradientShader = new GLSLShader(Loader.loadTextFile(res.openRawResource(R.raw.mainvert)),
+                Loader.loadTextFile(res.openRawResource(R.raw.tapadd_frag)));
+
+
+
+        jacobiShader = new GLSLShader(Loader.loadTextFile(res.openRawResource(R.raw.mainvert)),
+                Loader.loadTextFile(res.openRawResource(R.raw.tapadd_frag)));
+
+
+
+
+
+
+
+    }
 
 
 
