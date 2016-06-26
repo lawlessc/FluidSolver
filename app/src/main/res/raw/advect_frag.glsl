13d733e64b4fbdd8acb72c5988e25bde7574d902
Christopher Lawless <lawless85@gmail.com>
2016-06-26 20:10:48 +0100

    not there yet

--- app/src/main/res/raw/advect_frag.glsl
+++ app/src/main/res/raw/advect_frag.glsl
@@ -10,6 +10,8 @@ varying vec2 v_texCoord;
 void main()
 {
     vec2  fragCoord = v_texCoord.xy;
+     vec2  fragCoord = gl_FragCoord.xy;
+
    //float solid = texture(textureUnit2, inversesize.xy  * fragCoord).x;
    //if (solid > 0) {
    //   gl_FragColor = vec4(0);
